ff39e216e9399cc89b3eea7184672dc8598bd9d2
dotnotation <dotinmotionproductions@gmail.com>
2022-01-31 19:31:14 -0500

    commit tree 3e2f01fcccd5444f5f05d68c5586159a58d0c4dc

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+class DigitalClicker extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0,
+        }
+    }
+
+    increaseClicked = () => {
+        this.setState(previousState => ({
+            timesClicked: previousState.timesClicked+1
+        }))
+    }
+
+    render(){
+        return(
+            <button onClick={this.increaseClicked}>{this.state.timesClicked}</button>
+        )
+    }
+
+}
+export default DigitalClicker
